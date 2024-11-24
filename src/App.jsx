d31f764aa2f1759153a7f9c4974102b90639e732
Nicolas Mendez <nicolasmendez1917@icloud.com>
2024-11-24 12:39:06 -0500

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.scss'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   )
 }
