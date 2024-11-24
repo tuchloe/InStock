bd797e445287d926042c62528a43d301413c2046
Nicolas Mendez <nicolasmendez1917@icloud.com>
2024-11-23 22:22:21 -0500

    header_finished(no router)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './Header.scss';
 import logo1x from '../../assets/Logo/InStock-Logo_1x.png'
 import logo2x from '../../assets/Logo/InStock-Logo_2x.png'
+import { NavLink } from 'react-router-dom'
+
+// Notes:
+// Still need to implement NavLink, but for that, the react 
+// router has to be finished
 
 
 const Header = () => {
