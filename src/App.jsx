f9ad610a73bbbfdb7ed634fcfaf9f296b0fec00c
i-am-lp <lp.phillips95@gmail.com>
2024-12-07 22:13:01 -0500

    updated app.jsx page to include the import for header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WarehouseList from "./components/Warehouses/warehouses.jsx"
 import IndividualWarehouse from "./components/WarehouseIndividual/warehouse-individual.jsx"
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/footer.jsx";
 import './App.css'
+
 
 function App() {
 
