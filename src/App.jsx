0d3958968759f3aabdd8559b77212c93462509a6
i-am-lp <lp.phillips95@gmail.com>
2024-12-07 23:47:03 -0500

    updated header/footer position in app.jsx. added into the routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WarehouseList from "./components/Warehouses/warehouses.jsx"
 import IndividualWarehouse from "./components/WarehouseIndividual/warehouse-individual.jsx"
-import Header from "./components/Header/Header.jsx";
+import Header from "./components/Header/header.jsx";
 import Footer from "./components/Footer/footer.jsx";
 import './App.css'
 
@@ -10,27 +10,29 @@ function App() {
 
   return (
     <>
-    <Header />
     <BrowserRouter>
 
     <Routes>
       
       <Route path="/" element={
         <>
+        <Header />
         <WarehouseList />
+        <Footer />
         </>
       } />
 
       <Route path="/:id" element={
         <>
+        <Header />
         <IndividualWarehouse />
+        <Footer />
         </>
       } />
 
     </Routes>
 
     </BrowserRouter>
-    <Footer />
 
     </>
   );
